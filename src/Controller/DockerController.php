39f00532539378bb8f1641282965e3fd9ed93ec6
Petouha <rachidgames2.rg@gmail.com>
2024-04-11 17:05:35 +0200

    first api test

---
+++ src/Controller/DockerController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+#[Route('/api', name: 'api_')]
+class DockerController extends AbstractController
+{
+    #[Route('/docker', name: 'app_docker', methods: ['POST'])]
+    public function index(Request $request): Response
+    {
+        $post = ($request->getContent());
+        // write the result in a file
+        $file = fopen("/tmp/here.txt", "w");
+        fwrite($file,$post);
+        fclose($file);
+
+        return new JsonResponse($post);
+    }
+}
